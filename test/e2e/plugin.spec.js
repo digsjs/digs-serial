5c5a3322d665eedeb7ca7f699aac99c9b436222d
Christopher Hiller <boneskull@boneskull.com>
2015-11-08 11:58:58 -0800

    some tests pass!!

--- test/e2e/plugin.spec.js
+++ test/e2e/plugin.spec.js
@@ -1,16 +1,41 @@
 'use strict';
 
 const common = require('digs-common');
-common.debug.enable('digs*');
-const createServer = require('digs');
 const Promise = common.Promise;
 const getPort = require('get-port');
 Promise.longStackTraces();
 
 describe(`digs-serial plugin`, () => {
-  let digs;
   let sandbox;
   let serverOpts;
+  let createDigsServer;
+
+  before(() => {
+    common.debug.enable('digs*');
+    createDigsServer = require('digs');
+  });
+
+  function createServer() {
+    return Promise.all([getPort(), getPort()])
+      .spread((coapPort, httpPort) => {
+        serverOpts.server.app.coap.port = coapPort;
+        serverOpts.connections.push({
+          port: httpPort
+        });
+        const digsSerialOpts = {};
+        digsSerialOpts[require.resolve('../../lib')] = {
+          config: {
+            dink: {
+              config: {
+                io: 'johnny-five/test/util/mock-firmata'
+              }
+            }
+          }
+        };
+        serverOpts.plugins.push(digsSerialOpts);
+        return createDigsServer(serverOpts);
+      });
+  }
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create('digs-serial plugin');
@@ -23,41 +48,44 @@ describe(`digs-serial plugin`, () => {
       },
       connections: [],
       plugins: []
-
     };
   });
 
-  afterEach(done => {
+  afterEach(() => {
     sandbox.restore();
-    if (digs) {
-      digs.stop(done);
-    } else {
-      done();
-    }
   });
 
   /* eslint prefer-arrow-callback:0 */
   it(`should register successfully`, function() {
     this.timeout(4000);
-    return expect(Promise.all([getPort(), getPort()])
-      .spread((coapPort, httpPort) => {
-        serverOpts.server.app.coap.port = coapPort;
-        serverOpts.connections.push({port: httpPort});
-        const digsSerialOpts = {};
-        digsSerialOpts[require.resolve('../../lib')] = {
-          config: {
-            dink: {
-              config: {
-                io: 'johnny-five/test/util/mock-firmata'
-              }
-            }
-          }
-        };
-        serverOpts.plugins.push(digsSerialOpts);
-        return createServer(serverOpts);
-      })).to.eventually.be.fulfilled
+    return expect(createServer()).to.eventually.be.fulfilled
       .then(server => {
         return server.stopAsync();
       });
+  });
+
+  describe(`exposed method`, () => {
+    let digs;
+
+    /* eslint prefer-arrow-callback:0 */
+    beforeEach(function() {
+      this.timeout(4000);
+      return createServer()
+        .then(_digs => {
+          digs = _digs;
+        });
+    });
+
+    afterEach(() => {
+      if (digs) {
+        return digs.stopAsync();
+      }
+    });
+
+    describe(`serialPorts()`, () => {
+      it(`should expose plugin method serialPorts()`, () => {
+
+      });
+    });
   });
 });
